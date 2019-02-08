6bc0591a7739793b04f6afd5bc7077cc4a3d3344
BastLast <bastien.last87@gmail.com>
2019-02-08 14:05:46 +0100

    Ajout d'une meilleur solution pour UVa Argus

--- UVa1203.py
+++ UVa1203.py
@@ -1,5 +1,5 @@
 import sys
-
+# CETTE SOLUTION NE FONCTIONNE PAS PARFAITEMENT. Il serait préférable d'utiliser heap
 lines = sys.stdin.readlines()
 count = 0
 numbers = {}
@@ -26,3 +26,23 @@ for line in lines:
             todisplay = []
     if "#" in lineTab[0] :
         count = 1
+
+# Solution à privilegier
+import sys
+from _heapq import heappush, heappop
+lines = sys.stdin.readlines()
+if lines[-1] == '':
+    del lines[-1]
+h = []
+rep = 0
+for line in lines:
+    tableau = line.split()
+    if len(tableau) == 3:
+        heappush(h, (int(tableau[2]), int(tableau[1]), int(tableau[2])))
+    if len(tableau) == 1 and tableau[0] != "#":
+        rep = int(tableau[0])
+while rep > 0:
+     temp = heappop(h)
+     print(temp[1])
+     heappush(h, (int(temp[0]+temp[2]), int(temp[1]), int(temp[2])))
+     rep = rep - 1
