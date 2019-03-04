26284fb4a91871493a480f769519cc8d9ae6df51
BastLast <bastien.last87@gmail.com>
2019-03-04 16:19:59 +0100

    Ajout calculette 'partial accept'

---
+++ calculette.py
@@ -0,0 +1,35 @@
+import sys
+
+lines = sys.stdin.readlines()
+nb = []
+for line in lines :
+    line = line.rstrip('\n')
+    lineTab = line.split(" ")
+    resultat = -1
+    erreur = 0
+    for i in lineTab :
+        if i == '*' or i == '+' :
+            if len(nb) < 1 :
+                erreur = 1
+            if erreur == 0 :
+                if len(nb) == 1 :
+                    if resultat == -1 :
+                        erreur = 1
+                    else :
+                        nb1 = resultat
+                else:
+                    nb1 = nb.pop()
+                nb2 = nb.pop()
+                if i == '*':
+                    resultat = nb1 * nb2
+                else :
+                    resultat = nb1 + nb2
+        else :
+            nb.append(int(i))
+    if erreur == 1:
+        print("Expression invalide")
+    else :
+        if(len(nb)== 1) :
+            print(nb.pop())
+        else:
+            print(resultat)
